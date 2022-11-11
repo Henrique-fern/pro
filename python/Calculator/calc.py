33d495c9d2af214bc5e95461ccd8142ae9143a50
Henrique-fern <118005249+Henrique-fern@users.noreply.github.com>
2022-11-11 16:35:50 +0000

    Add files via upload

---
+++ python/Calculator/calc.py
@@ -0,0 +1,49 @@
+from calc_art import logo
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+
+}
+
+
+def calculator():
+    print(logo)
+
+    num1 = float(input("What's the first number? "))
+    for i in operations:
+        print(i)
+    status = True
+    
+    while status:
+        operation_symbol= input("Pick an operation: ")
+        num2 = float(input("What's the next number? "))
+        calculation_funcion = operations[operation_symbol]
+        answer = calculation_funcion(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        again = input(f"Type 'y' to calculate calculating with {answer}, type 'n' to start a new calculation, or type 'exit' to finish :  ")
+        if again == 'y':
+            num1 = answer
+        elif again == 'n':
+            status = False
+            calculator()
+        else:
+            status = False
+
+calculator()
